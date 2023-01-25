165e03a68a86ce936c560c422c144f6fbd1a25c0
Marcio Jose Alves <marciojosealves2@gmail.com>
2023-01-25 17:23:17 -0300

    commit inicial

---
+++ Fazendo o basico de matematica.py
@@ -0,0 +1,35 @@
+# Fazendo o básico
+
+# Declarando variáveis
+variavel_um = 2
+variavel_dois = 2
+variavel_tres = 7
+variavel_quatro = 3.5
+variavel_cinco = 4
+variavel_seis = 3
+
+'''Em Python pode-se fazer outras expressões mais complexas
+e os operadores básicos são + adição, - subtração, * multiplicação, / divisão.'''
+
+# Uma soma simples...
+soma_variaveis = variavel_um + variavel_dois
+
+# Uma divisão simples
+divisao_variaveis = variavel_tres/variavel_quatro
+
+# Uma multiplicação simples
+multiplicacao_variaveis = variavel_tres*variavel_quatro
+
+# Quociente inteiro de divisão
+quociente_inteiro_divisão = variavel_cinco//variavel_seis
+
+# Resto de divisão
+resto_divisão = variavel_tres % variavel_seis
+
+# Uma declaração simples
+feito_por = "MJA"
+
+# Apresentação dos resultado na tela
+print("Soma:", soma_variaveis, ";", "divisão:", divisao_variaveis,
+      ";", "multiplicação:", multiplicacao_variaveis, ";", "Quociente inteiro:", quociente_inteiro_divisão, ";", "Resto divisão inteira:", resto_divisão, ".")
+print("Potenciação:", variavel_tres**2, ". Feito por:", feito_por)
